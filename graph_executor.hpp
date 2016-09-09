91a2aa35171bb73f3c3c7f5bef7da724ecb1ddc4
Kristjan Kongas <kongaskristjan@gmail.com>
2016-09-09 16:19:28 +0300

    Modift Graph_executor interface to allow pushing results

--- graph_executor.hpp
+++ graph_executor.hpp
@@ -12,7 +12,9 @@
       Graph_executor's permission to run asyncronously (but
       it is not obliged to run asyncronously)
   
-  push_job() - push a job to be processed
+  push_back() - push a job to be processed or
+      a result to be used for initialization or to fill space
+  
   sync(i) - force Graph_executor to finish jobs before i.
       Will not return before everything is done.
   clear(i) - release all results before i as soon as possible
@@ -28,8 +30,10 @@ public:
     virtual void set_async_mode(bool) = 0;
     virtual bool async_mode() const = 0;
     
-    virtual std::uint64_t push_job(std::unique_ptr<Job> job,
-                                   const std::vector<std::uint64_t> & args) = 0;
+    virtual std::uint64_t push_back(std::unique_ptr<Job> job,
+                                    const std::vector<std::uint64_t> & args) = 0;
+    virtual std::uint64_t push_back(std::unique_ptr<Result>) = 0;
+    
     virtual void sync() = 0;
     virtual void sync(std::uint64_t) = 0;
     virtual void clear() = 0;

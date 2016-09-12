728121e6160d68dea7dc4453c13347e0b74903c2
Kristjan Kongas <kongaskristjan@gmail.com>
2016-09-12 11:18:13 +0300

    Update Graph_executor interface and remove some unnecessary "std::".

--- graph_executor.hpp
+++ graph_executor.hpp
@@ -12,19 +12,23 @@
       Graph_executor's permission to run asyncronously (but
       it is not obliged to run asyncronously)
   
-  push_back() - push a job to be processed or
+  push() - push a job to be processed or
       a result to be used for initialization or to fill space
   
-  sync(i) - force Graph_executor to finish all jobs j <= i.
+  sync(i) - force Graph_executor to finish all jobs j < i.
       Will not return before everything is done.
       If i is not specified, sync everything submitted.
-  clear(i) - release all results j <= i as soon as possible
+  clear(i) - release all results j < i as soon as possible
       If i is not specified, clear everything submitted that is independent.
   
-  operator[] - return result. If answer is not yet calculated,
-      wait until it is calculated.
+  operator[] - return result. In general avoid storing the reference/pointer,
+      but if you must, don't clear before the reference is destroyed.
+      If answer is not yet calculated, it will wait until it is calculated.
   hand_over() - hand result over to the main process. If answer is
-      not yet calculated, wait until it is calculated.
+      not yet calculated, it will wait until it is calculated.
+
+  Graph_executor assumes that no threads are explicitly created by any
+      jobs. Jobs may however be created by main thread or other jobs.
 */
 
 
@@ -33,18 +37,17 @@ public:
     virtual void set_async_mode(bool) = 0;
     virtual bool async_mode() const = 0;
     
-    virtual std::uint64_t push_back(std::unique_ptr<Job> job,
-                                    const std::vector<std::uint64_t> & args) = 0;
-    virtual std::uint64_t push_back(std::unique_ptr<Result>) = 0;
+    virtual uint64_t push(std::unique_ptr<Job> job,
+                          const std::vector<uint64_t> & args) = 0;
+    virtual uint64_t push(std::unique_ptr<Result>) = 0;
     
     virtual void sync() = 0;
-    virtual void sync(std::uint64_t) = 0;
+    virtual void sync(uint64_t) = 0;
     virtual void clear() = 0;
-    virtual void clear(std::uint64_t) = 0;
+    virtual void clear(uint64_t) = 0;
     
-    virtual const Result & operator[](std::uint64_t) const = 0;
-    virtual Result & operator[](std::uint64_t) = 0;
-    virtual std::unique_ptr<Result> hand_over(std::uint64_t) = 0;
+    virtual const Result & operator[](uint64_t) = 0;
+    virtual std::unique_ptr<Result> hand_over(uint64_t) = 0;
     
     virtual inline ~Graph_executor();
 };

d273756216a2e2ff2b0e0993cb928a2748837c2d
Kristjan Kongas <kongaskristjan@gmail.com>
2016-09-12 11:21:27 +0300

    Make some elementary testing functions in main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 
 #include <graph_executor.hpp>
+#include <single_graph_executor.hpp>
 #include <example_add.hpp>
 #include <iostream>
 #include <memory>
@@ -22,8 +23,32 @@ void test_add()
 }
 
 
+void test_graph_executor(Graph_executor & executor)
+{
+    // Generate Fibonacci numbers
+    const int n = 10;
+    
+    std::vector<uint64_t> indeces;
+    indeces.push_back(executor.push(std::unique_ptr<Result>(new Integer(0))));
+    indeces.push_back(executor.push(std::unique_ptr<Result>(new Integer(1))));
+    for (int i = 2; i <= n; ++i)
+        indeces.push_back(
+            executor.push(std::unique_ptr<Job>(new Add),
+                          std::vector<uint64_t>
+                          ({indeces[i - 2], indeces[i - 1]})));
+
+    for (uint64_t index: indeces){
+        std::cout << "index: " << index << "\n";
+        std::cout << "ans: " << static_cast<const Integer &>(executor[index]).x << "\n";
+    }
+}
+
+
 int main()
 {
     test_add();
+    std::unique_ptr<Graph_executor> executor(new Single_graph_executor(false));
+    test_graph_executor(* executor.get());
+    
     return 0;
 }

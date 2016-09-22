55d1d7fa7aeedca0fbe88a175a8f7d30f332efcc
Kristjan Kongas <kongaskristjan@gmail.com>
2016-09-22 18:26:49 +0300

    Add force_hand_over() to Graph_executor

--- graph_executor.hpp
+++ graph_executor.hpp
@@ -19,9 +19,18 @@
       is not yet calculated, it will wait until it is calculated.
   hand_over() - hand result over to the main process. If answer is
       not yet calculated, it will wait until it is calculated.
+      If answer is used by another job, Graph_executor waits until it's done.
+  force_hand_over() - Same as hand_over, but doesn't wait if other
+      jobs might use it according to dep_count.
 
-  Graph_executor assumes that no threads are explicitly created by any
+  Graph_executor assumes at the moment, that no jobs are created within
+      jobs themselves. However, this might change later.
+
+  The following might be used as an expansion.
+  """Graph_executor assumes that no threads are explicitly created by any
       jobs. Jobs may however be created by main thread or other jobs.
+      However, those results may not be used (the only arguments used
+      must be those given as arguments)."""
 */
 
 
@@ -34,6 +43,7 @@ public:
     
     virtual const Result & operator[](uint64_t) = 0;
     virtual std::unique_ptr<Result> hand_over(uint64_t) = 0;
+    virtual std::unique_ptr<Result> force_hand_over(uint64_t) = 0;
     
     virtual inline ~Graph_executor();
 };

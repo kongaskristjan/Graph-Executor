df2b4c74accf2f6e697084f943d8310e6d335c54
Kristjan Kongas <kongaskristjan@gmail.com>
2016-09-22 22:59:48 +0300

    Add Random_add test to Perform::tests

--- perform_tests.cpp
+++ perform_tests.cpp
@@ -1,23 +1,40 @@
 
 #include <perform_tests.hpp>
 
-void Perform_tests::all(Tester & tester)
+void Perform_tests::all(Tester & tester, const size_t n)
 {
-    fibonacci(tester);
+    fibonacci(tester, n);
+    random_add(tester, n);
 }
 
 
-void Perform_tests::fibonacci(Tester & tester, const int n)
+void Perform_tests::fibonacci(Tester & tester, const size_t n)
 {
-    // Generate Fibonacci numbers
+    tester.push(Integer(0));
+    tester.push(Integer(1));
     
-    std::vector<uint64_t> idxs;
-    idxs.push_back(tester.push(Integer(0)));
-    idxs.push_back(tester.push(Integer(1)));
-    
-    for (int i = 2; i <= n; ++i)
-        idxs.push_back(tester.push(Add(), std::vector<uint64_t>
-                                   ({idxs[i - 2], idxs[i - 1]})));
+    for (size_t i = 2; i <= n; ++i)
+        tester.push(Add(), std::vector<uint64_t>({i - 2, i - 1}));
     
     tester.verify_and_end_test();
+}
+
+
+void Perform_tests::random_add(Tester & tester, const size_t n, const size_t k)
+{
+    auto now = std::chrono::system_clock::now();
+    unsigned seed = std::chrono::duration_cast<std::chrono::nanoseconds>
+        (now.time_since_epoch()).count();
+    std::default_random_engine gen(seed);
+    
+    for (size_t i = 0; i < k; ++i)
+        tester.push(Integer(i + 1));
+
+    for (size_t i = k; i < k + n; ++i){
+        std::uniform_int_distribution<uint64_t> distr(0, i - 1);
+
+        uint64_t idx0 = distr(gen);
+        uint64_t idx1 = distr(gen);
+        tester.push(Add(), std::vector<uint64_t>({idx0, idx1}));
+    }
 }
--- perform_tests.hpp
+++ perform_tests.hpp
@@ -4,13 +4,17 @@
 #include <tester.hpp>
 #include <example_add.hpp>
 #include <example_cpu.hpp>
+#include <vector>
+#include <random>
+#include <chrono>
 
 class Perform_tests {
 public:
-    static void all(Tester &);
+    static void all(Tester &, const size_t n = 10);
     
     // Assumes Tester has ended last test
-    static void fibonacci(Tester &, const int n = 10);
+    static void fibonacci(Tester &, const size_t n = 10);
+    static void random_add(Tester &, const size_t n = 10, const size_t k = 2);
 };
 
 #endif
